69d5ac548b90859a4c981b45e1c548d24ab497c5
josedoce <josesamya@gmail.com>
2021-06-25 15:17:40 -0300

    auth: refatoracão

--- src/routes/auth.routes.tsx
+++ src/routes/auth.routes.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 
 import { Home } from "../screens/Home";
-import { SignIn } from "../screens/Signin";
 import { AppointmentDetails } from '../screens/AppointmentDetails';
 import { AppointmentCreate } from "../screens/AppointmentCreate";
 import { colors } from "../global/styles/theme";
@@ -19,11 +18,7 @@ export function AuthRoutes(){
         }
       }}
     >
-      <Screen 
-        name="Signin" 
-        component={SignIn}
-      />
-
+    
       <Screen 
         name="Home" 
         component={Home}
