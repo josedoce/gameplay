04c09694db77d8aad8bbf2f41fa66d03f24042f1
josedoce <josesamya@gmail.com>
2021-06-25 15:17:10 -0300

    auth: configs de auteticação

---
+++ src/configs/discordAuth.ts
@@ -0,0 +1,7 @@
+export enum discord {
+  REDIRECT_URI = 'https://auth.expo.io/@josenervoso/gameplay',
+  SCOPE = 'identify%20email%20connections%20guilds',
+  RESPONSE_TYPE = 'token',
+  CLIENT_ID = '857992766720966656',
+  CDN_IMAGE = 'https://cdn.discordapp.com'
+}
---
+++ src/configs/index.ts
@@ -0,0 +1,3 @@
+import { discord } from "./discordAuth";
+
+export {discord};
