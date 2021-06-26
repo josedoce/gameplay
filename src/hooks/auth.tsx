a1ffa9b42d43a034119d129a2bc9bcc1c17440b0
josedoce <josesamya@gmail.com>
2021-06-26 14:46:08 -0300

    fim: melhorias e limpeza de codigo

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -2,11 +2,19 @@ import React, {
   createContext,
   useContext,
   useState,
-  ReactNode
+  ReactNode,
+  useEffect
 } from 'react';
 import * as AuthSession from 'expo-auth-session';
-import { discord } from '../configs';
 import { api } from '../services/api';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { COLLECTION_USERS } from '../configs/database';
+
+const {REDIRECT_URI} = process.env;
+const {SCOPE} = process.env
+const {RESPONSE_TYPE} = process.env
+const {CLIENT_ID} = process.env
+const {CDN_IMAGE} = process.env
 
 type User = {
   id: string;
@@ -21,6 +29,7 @@ type AuthContextData = {
   user: User;
   loading: boolean;
   signIn: () => Promise<void>;
+  signOut: () => Promise<void>;
 }
 
 type AuthProviderProps = {
@@ -29,16 +38,20 @@ type AuthProviderProps = {
 
 type AuthorizationResponse = AuthSession.AuthSessionResult & {
   params: {
-    access_token: string;
+    access_token?: string;
+    error?: string;
   }
 }
-
+/**
+ * contexto da aplicação
+*/
 export const AuthContext = createContext({} as AuthContextData);
 
 function AuthProvider({children}:AuthProviderProps){
   const [user, setUser] = useState<User>({} as User);
   const [loading, setLoading] = useState(false)
-  const authUrl = `${api.defaults.baseURL}/oauth2/authorize?client_id=${discord.CLIENT_ID}&redirect_uri=${discord.REDIRECT_URI}&response_type=${discord.RESPONSE_TYPE}&scope=${discord.SCOPE}`;
+  const authUrl = `${api.defaults.baseURL}/oauth2/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}&scope=${SCOPE}`;
+  
   async function signIn(){
     try {
       setLoading(true);
@@ -47,31 +60,55 @@ function AuthProvider({children}:AuthProviderProps){
         type
       } = await AuthSession.startAsync({ authUrl }) as AuthorizationResponse;
       
-      if(type === 'success'){
+      if(type === 'success'&&!params.error){//se deu sucesso e não cancelou a autorização, continue
         api.defaults.headers.authorization = `Bearer ${params.access_token}`;
         const userInfo = await api.get('/users/@me');
         const firstName = userInfo.data.username.split(' ')[0];
-        userInfo.data.avatar = `${discord.CDN_IMAGE}/avatars/${userInfo.data.id}/${userInfo.data.avatar}.png`;
-        setUser({
+        userInfo.data.avatar = `${CDN_IMAGE}/avatars/${userInfo.data.id}/${userInfo.data.avatar}.png`;
+        
+        const userData = {
           ...userInfo.data,
           firstName,
           token: params.access_token
-        });
-        setLoading(false);
-      }else{
-        setLoading(false);
+        }
+
+        await AsyncStorage.setItem(COLLECTION_USERS, JSON.stringify(userData));
+
+        //aqui seta os dados vindos da api
+        setUser(userData);
       }
 
     } catch {
       throw new Error('Não foi possivel autenticar.');
+    } finally {
+      setLoading(false);
     }
   }
 
+  async function signOut(){
+    setUser({} as User);
+    await AsyncStorage.removeItem(COLLECTION_USERS);
+  }
+
+  async function loadUserStorageData(){
+    const storage = await AsyncStorage.getItem(COLLECTION_USERS);
+
+    if(storage){
+      const userLogged = JSON.parse(storage) as User;
+      api.defaults.headers.authorization = `Bearer ${userLogged.token}`;
+      setUser(userLogged);
+    }
+  }
+
+  useEffect(()=>{
+    loadUserStorageData();
+  },[]);
   return (
     <AuthContext.Provider value={{
       user,
       loading,
-      signIn
+      signIn,
+      signOut
     }}>
       {children}
     </AuthContext.Provider>
