44af46ecd51203f8e754192b5f2acf2e9c9175c3
josedoce <josesamya@gmail.com>
2021-06-26 14:44:02 -0300

    fim: remoção de comentarios

--- src/@types/svg.d.ts
+++ src/@types/svg.d.ts
@@ -1,6 +1,6 @@
 declare module "*.svg" {
-  import React from "react"; //importe o react
-  import {SvgProps} from 'react-native-svg'; //import o tipo svg
-  const content: React.FC<SvgProps>; //avise ao React que o tipo svg é um component funcional
-  export default content; //exporte para o react essa declaração
+  import React from "react";
+  import {SvgProps} from 'react-native-svg';
+  const content: React.FC<SvgProps>;
+  export default content;
 };
