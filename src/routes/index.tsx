01c3d97a4b54742bba6e3bff897c5706fecfac40
josedoce <josesamya@gmail.com>
2021-06-26 14:46:33 -0300

    edit: auth.routes para app.routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,12 @@
 import React from "react";
 import {NavigationContainer} from '@react-navigation/native';
-import { AuthRoutes } from "./auth.routes";
+import { AuthRoutes } from "./app.routes";
 import { SignIn } from "../screens/Signin";
 import { useAuth } from "../hooks/auth";
 
-//se tem usuario logado, ele avança pras telas, caso contrario, fica no signin
 export function Routes(){
   const {user} = useAuth();
-  return(//essa parte cuida da logica de paginação
+  return(
     <NavigationContainer>
       {user.id?<AuthRoutes/>:<SignIn/>}
     </NavigationContainer>
