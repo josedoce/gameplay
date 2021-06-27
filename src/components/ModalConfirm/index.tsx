2e302c36cca9771e9527f72226cad37676c30162
josedoce <josesamya@gmail.com>
2021-06-27 17:21:12 -0300

    modal: modal customizado

--- src/components/ModalConfirm/index.tsx
+++ src/components/ModalConfirm/index.tsx
@@ -7,25 +7,27 @@ import { ModalButton } from '../ModalButton';
 import { styles } from './styles';
 
 type ModelLogoutProps = {
+  title: string;
+  buttonsOneandTwo?: string[];
   closeModel: ()=>void;
   actionModel: ()=>void;
 }
 
-export function ModalLogout({closeModel, actionModel}:ModelLogoutProps){
+export function ModalConfirm({title,buttonsOneandTwo=['cancelar','confirmar'], closeModel, actionModel}:ModelLogoutProps){
   return (
     <>
-    <Text style={styles.title}>Deseja fazer logout?</Text>
+    <Text style={styles.title}>{title}</Text>
     <View style={styles.container}> 
        <View style={styles.botoes}>
           <ModalButton
             onPress={closeModel}
             outline 
-            title="cancelar"/>
+            title={buttonsOneandTwo[0]}/>
        </View>
        <View style={styles.botoes}>
         <ModalButton
           onPress={actionModel} 
-          title="confirmar"/>
+          title={buttonsOneandTwo[1]}/>
        </View>
     </View>
     </>
